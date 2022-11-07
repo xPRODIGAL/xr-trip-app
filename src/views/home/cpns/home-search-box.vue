fc720cd2870bc7c6f6f18e66c7e9c4aa80bba1e6
xr <a365856177@163.com>
2022-11-07 15:54:07 +0800

    日期处理

--- src/views/home/cpns/home-search-box.vue
+++ src/views/home/cpns/home-search-box.vue
@@ -1,23 +1,157 @@
+<script setup>
+import { ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import { formatMonthDay, getDiffDays } from '@/utils/format_date'
+import useDateStore from '@/stores/modules/date'
+import { computed } from '@vue/reactivity';
+
+
+// 日期相关处理
+const dateStore = useDateStore()
+const { startDate, endDate } = storeToRefs(dateStore)
+
+const startDateStr = computed(() => formatMonthDay(startDate.value))
+const endDateStr = computed(() => formatMonthDay(endDate.value))
+const stayCount = ref(getDiffDays(startDate.value, endDate.value))
+
+const showCalendar = ref(false)
+const onConfirm = (value) => {
+  // 1.设置日期
+  const selectStartDate = value[0]
+  const selectEndDate = value[1]
+  dateStore.startDate = selectStartDate
+  dateStore.endDate = selectEndDate
+  stayCount.value = getDiffDays(selectStartDate, selectEndDate)
+
+  // 2.隐藏日历
+  showCalendar.value = false
+}
+
+
+</script>
+
 <template>
   <div class="search-box">
     <!-- 位置信息 -->
-    <div class="location">
+    <div class="location bottom-gray-line">
       <div class="city">南京</div>
       <div class="position">
         <span class="text">我的位置</span>
         <img src="@/assets/img/home/icon_location.png" alt="" />
       </div>
     </div>
-    
+
     <!-- 日期范围 -->
+    <div class="section date-range" @click="showCalendar = true">
+      <div class="start">
+        <div class="date">
+          <span class="tip">入住</span>
+          <span class="time">{{ startDateStr }}</span>
+        </div>
+        <div class="stay">共{{ stayCount }}晚</div>
+      </div>
+      <div class="end">
+        <div class="date">
+          <span class="tip">离店</span>
+          <span class="time">{{ endDateStr }}</span>
+        </div>
+      </div>
+    </div>
+    <van-calendar
+      v-model:show="showCalendar"
+      type="range"
+      color="#ff9854"
+      :round="false"
+      :show-confirm="false"
+      @confirm="onConfirm"
+    />
 
     <!-- 价格人数 -->
   </div>
 </template>
 
-<script setup>
-
-</script>
-
 <style lang="less" scoped>
+.search-box {
+  --van-calendar-popup-height: 100%;
+
+  .location {
+    display: flex;
+    align-items: center;
+    height: 44px;
+    padding: 0 20px;
+
+    .city {
+      flex: 1;
+      color: #333;
+      font-size: 15px;
+    }
+
+    .position {
+      width: 74px;
+      display: flex;
+      align-items: center;
+
+      .text {
+        position: relative;
+        top: 2px;
+        color: #666;
+        font-size: 12px;
+      }
+
+      img {
+        margin-left: 5px;
+        width: 18px;
+        height: 18px;
+      }
+    }
+  }
+
+  .section {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    padding: 0 20px;
+    color: #999;
+    height: 44px;
+
+    .start {
+      flex: 1;
+      display: flex;
+      height: 44px;
+      align-items: center;
+    }
+
+    .end {
+      min-width: 30%;
+      padding-left: 20px;
+    }
+
+    .date {
+      display: flex;
+      flex-direction: column;
+
+      .tip {
+        font-size: 12px;
+        color: #999;
+      }
+
+      .time {
+        margin-top: 3px;
+        color: #333;
+        font-size: 15px;
+        font-weight: 500;
+      }
+    }
+  }
+
+  .date-range {
+    height: 44px;
+    .stay {
+      flex: 1;
+      text-align: center;
+      font-size: 12px;
+      color: #666;
+    }
+  }
+}
 </style>

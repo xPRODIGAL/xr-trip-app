6d359ff2771b8c9029480b906c804e86adc55e20
xr <a365856177@163.com>
2022-11-09 18:20:13 +0800

    城市相关处理-map动态索引

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,16 +1,65 @@
 <template>
-  <div class="city">
-    <template v-for="item in 100">
-      <div>
-        hahah 
-      </div>
-    </template>
+  <div class="city top-page">
+    <div class="top">
+      <!-- 1.搜索框 -->
+      <van-search 
+        v-model="searchValue" 
+        placeholder="城市/区域/位置"
+        shape="round"
+        show-action
+        @cancel="cancelClick"
+      />
+
+      <!-- 2.tab的切换 -->
+      <!-- tabActive默认索引 -->
+      <van-tabs v-model:active="tabActive" color="#ff9854" line-height="3">
+        <template v-for="(value, key, index) in allCities" :key="key">
+          <van-tab :title="value.title" :name="key"></van-tab>
+        </template>
+      </van-tabs>
+    </div>
+    <div class="content">
+      <template v-for="(value, key, index) in allCities">
+        <!-- <h2 v-show="tabActive === key">{{ value.title }}</h2> -->
+        <city-group v-show="tabActive === key" :group-data="value" />
+      </template>
+    </div>
   </div>
 </template>
 
 <script setup>
+import CityGroup from './cpns/city-group.vue'
+import useCityStore from '@/stores/modules/city';
+import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+const searchValue = ref("")
+const cancelClick = () => {
+  router.back()
+}
+
+const tabActive = ref()
+
+const cityStore = useCityStore()
+cityStore.fetchAllCitiesData()
+const { allCities } = storeToRefs(cityStore)
 
 </script>
 
 <style lang="less" scoped>
+.city {
+  .top {
+    position: relative;
+    z-index: 9;
+  }
+
+  // 布局滚动
+  .content {
+    height: calc(100vh - 98px);
+    overflow-y: auto;
+  }
+}
 </style>

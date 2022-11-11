336b0cfe233869b5a372208db726cf5f87fba356
xr <a365856177@163.com>
2022-11-11 11:38:23 +0800

    移动端适配

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
-import { VantResolver } from 'unplugin-vue-components/resolvers';
+import { VantResolver } from 'unplugin-vue-components/resolvers'
+import pxtovw from 'postcss-px-to-viewport'
+
+const loder_pxtovw = pxtovw({
+  viewportWidth: 375,
+  viewportUnit: 'vw'
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +19,11 @@ export default defineConfig({
       resolvers: [VantResolver()],
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [loder_pxtovw]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
